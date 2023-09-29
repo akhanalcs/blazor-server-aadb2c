b122764ea0e541cf838acdf7831492cc4ff36512
Ashish Khanal <affableashk@gmail.com>
2023-09-29 15:18:22 -0400

    Adds working(net7) and non-working projects (net8)

--- MunsonPickles.Shared/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace MunsonPickles.Shared.Models;
-
-public class Product
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string? Description { get; set; }
-    public string? ProductType { get; set; }
-}
